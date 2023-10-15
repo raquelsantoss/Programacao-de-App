263a4a8eb14d31fa2ccf81c95451b837ff249ede
Raquel Santos <raquel.pri@hotmail.com>
2023-10-15 20:41:44 -0300

    feat: creating first project

---
+++ app/src/main/java/com/example/todolist/data/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.todolist.data.util
+
+class Constants {
+    companion object{
+        const val MAX_TIMESTAMP = 8640000000000000
+        const val sDate = 1640908800
+    }
+}
